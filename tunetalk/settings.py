39a62452e7cb815bae6aedcb16a031006eb0ecc9
Arianne Santiago <ariannesantiago05@gmail.com>
2024-10-16 10:18:46 +0000

    Add missing typed files for pip vendor packages

--- tunetalk/settings.py
+++ tunetalk/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['8000-ariannesantiag-tunetalk-mj9fpwicigr.ws-eu110.gitpod.io','.herokuapp.com','8000-ariannesantiag-tunetalk-2kugvsgi3fg.ws.codeinstitute-ide.net']
